ec4e8d6de7dd0c0dfd3df64f3089a0012db663f7
DeclanG9 <kanozx23@gmail.com>
2022-12-20 22:45:08 +0700

    updateNew

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone --depth 1 https://github.com/DeclanG9/local_manifest -b new_derp .repo/local_manifests
+git clone --depth 1 https://github.com/DeclanG9/local_manifest -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
